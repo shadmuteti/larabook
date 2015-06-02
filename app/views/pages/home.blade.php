eaf69097c10488cdce8bb0a7efd4ae8d8cedc52b
shady <shadmuteti2@gmail.com>
2015-06-02 09:43:11 +0300

    larabook example added

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+    <div class="jumbotron">
+   <h1> welcome to larabook !</h1>
+        <p> sign up for laravel and see all what the fuss is all about</p>
+    <p><a class="btn btn-lg btn-primary" href="../..component/#navbar" role="button">Sign up</a></p>
+    </div>
+@stop
